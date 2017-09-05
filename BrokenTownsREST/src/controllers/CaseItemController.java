91f23dbd72d89c9e95ac11b8e1b2939c35a7736a
Charlie Actor <Charlie@Charless-MacBook-Pro.local>
2017-09-05 10:31:51 -0600

    case item controller and dao, also inserted beans into servlet

---
+++ BrokenTownsREST/src/controllers/CaseItemController.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import data.CaseDAO;
+import entities.CaseItem;
+
+@RestController
+public class CaseItemController {
+	
+	@Autowired
+	private CaseDAO dao;
+	
+	@RequestMapping(path = "ping", method = RequestMethod.GET)
+	public String ping() {
+		return "pong";
+	}
+	
+	@RequestMapping(path = "caseItems", method = RequestMethod.GET)
+	public Set<CaseItem> index() {
+		return dao.index();
+	}
+	
+	@RequestMapping(path = "caseItems/{id}", method = RequestMethod.GET)
+	public CaseItem show(@PathVariable int id) {
+		return dao.show(id);
+	}
+	
+	@RequestMapping(path = "caseItems", method = RequestMethod.POST)
+	public CaseItem create(@RequestBody String jsonCaseItem) {
+		return dao.create(jsonCaseItem);
+	}
+	
+	@RequestMapping(path = "caseItems/{id}", method = RequestMethod.PUT)
+	public CaseItem update(@PathVariable int id, @RequestBody String jsonCaseItem) {
+		return dao.update(id, jsonCaseItem);
+	}
+	
+	@RequestMapping(path = "caseItems/{id}", method = RequestMethod.DELETE)
+	public boolean destroy(@PathVariable int id) {
+		return dao.destroy(id);
+	}
+
+}
