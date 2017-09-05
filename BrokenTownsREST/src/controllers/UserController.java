7d0808fab5ba9be3dad11d765e80145e43fb4910
broberson7801 <broberson7801@gmail.com>
2017-09-05 10:04:40 -0600

    stuff

--- BrokenTownsREST/src/controllers/UserController.java
+++ BrokenTownsREST/src/controllers/UserController.java
@@ -1,16 +1,44 @@
 package controllers;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import data.UserDAO;
+import entities.User;
+
 @RestController
 public class UserController {
+	
+	@Autowired
+	private UserDAO dao;
 	
 	@RequestMapping(path = "ping", method = RequestMethod.GET)
 	public String ping() {
 		return "pong";
 	}
 	
-
+	@RequestMapping(path = "/users", method = RequestMethod.GET)
+	public Set<User> index(HttpServletRequest req, HttpServletResponse res) {
+		return dao.index();
+	}
+	
+	@RequestMapping(path = "/users/{id}", method = RequestMethod.GET) 
+	public User show(HttpServletRequest req, HttpServletResponse res, @PathVariable int id) {
+		return dao.show(id);
+	}
+	
+	@RequestMapping(path = "/users", method = RequestMethod.POST)
+	public User creatre(HttpServletRequest req, HttpServletResponse res, @RequestBody String userJson) {
+		return dao.create(userJson);
+	}
+	
 }
