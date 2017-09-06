1bb8573888e10e7d57dd59d39a5944f9a40cde2c
Charlie Actor <Charlie@Charless-MacBook-Pro.local>
2017-09-06 10:45:45 -0600

    angular Municipality and caseItem service

---
+++ BrokenTownsREST/WebContent/app/appModule/caseItem.service.js
@@ -0,0 +1,77 @@
+angular.module('appModule')
+	  .factory('caseItemService', function($http, authService, $location) {
+		  var service = {};
+		  
+		  var BASE_URL = "http://localhost:8080/ngTodo/api/caseItems/";
+		  
+		  var token = null;
+		   
+		  var refreshToken = function() {
+		    token = authService.getToken();
+		  };
+	   
+		  refreshToken();
+		  
+		  var checkLogin = function() {
+			  var loggedInUser = authService.getToken();
+			  if (loggedInUser == null) {
+				  $location.path("/login");
+			  }
+		  }
+		  
+		  service.index = function() {
+			  checkLogin();
+			  refreshToken();
+			  
+			  return $http({
+				  method  : "GET", 
+				  url     : BASE_URL
+			  });
+		  }
+		  
+		  service.create = function(caseItem) {
+			  checkLogin();
+			  refreshToken();
+			  caseItem.done = false;
+			  return $http({
+				  method   :   "POST",
+				  url      :   BASE_URL,
+				  headers  : {
+					  'Content-Type'  :  'application/json'
+				  },
+				  data     : caseItem
+			  });
+		  }
+		  
+		  service.update = function(caseItem) {
+			  checkLogin();
+			  refreshToken();
+			  var id = caseItem.id;
+			  /*
+			   caseItem.completeDate = "";
+			   if (caseItem.done == true) {
+			   	caseItem.completeDate = $filter('date')(Date.now(), 'MM/dd/yyyy');
+			   }
+			   */
+			  return $http({
+				  method   :  "PUT",
+				  url      :  BASE_URL + id,
+				  headers  : {
+					  'Content-Type' : "application/json"
+				  },
+				  data     : caseItem
+			  });
+		  }
+		  
+		  service.destroy = function(caseId) {
+			  checkLogin();
+			  refreshToken();
+			  return $http({
+				  method    :   "DELETE",
+				  url       :   BASE_URL + caseId
+			  });
+		  }
+		  
+		  return service;
+	  })
+	  
