fa4dca274b607c6186067474498efc278d454801
broberson7801 <broberson7801@gmail.com>
2017-09-11 13:33:06 -0600

    stuff

--- BrokenTownsREST/WebContent/app/appModule/home.component.js
+++ BrokenTownsREST/WebContent/app/appModule/home.component.js
@@ -36,6 +36,72 @@ angular.module('appModule').component(
 				1200, 70
 
 				);
+				
+//				Login Stuff
+				vm.errors = [];
+				
+				vm.login = function(user) {
+					vm.errors = [];
+				
+					authService.login(user)
+						.then(function(res) {
+							if(res.data.municipality === null) {
+							$location.path('/user')
+							return true;
+							} else if(res.data.municipality !== null) {
+								$location.path('/municipality')
+								return true;
+							}
+				
+						})
+						.catch();
+						vm.errors.push("Your email and/or password is incorrect. Please Try Again");
+						return vm.errors;
+				}
+				
+				vm.invalid = function(user) {
+					if (!user) return true;
+					if (!user.email || !user.password || !user.confirm) return true;
+					return false;
+				}
+				
+//				Registration Stuff
+				vm.register = function(user) {
+					vm.errors = [];
+					
+					var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+					
+					if(!user.firstName) {
+						vm.errors.push("You must have a first name");
+					}
+					
+					if(!user.lastName) {
+						vm.errors.push("You must have a last name");
+					}
+					
+					
+					if (!re.test(user.email)) {
+						vm.errors.push("Your email is not an email");
+					}
+					if (!user.password || user.password.length < 6) {
+						vm.errors.push("You must include a password, of at least 6 characters")
+					}
+					if (user.password !== user.confirm) {
+						vm.errors.push("Your passwords do not match");
+					}
+
+					if (vm.errors.length > 0) {
+						return;
+					}
+					
+					delete user.confirm;
+					
+					authService.register(user)
+						.then(function(res) {
+							$location.path('/login');
+						})
+						.catch(console.error)
+				}
 
 			},
 			controllerAs : 'vm'
