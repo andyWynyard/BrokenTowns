d2c658f056427dc1457f0728dc8d32dc866d19e5
Andrew Wynyard <andrew.wynyard@gmail.com>
2017-09-11 15:57:37 -0600

    stuff

--- BrokenTownsREST/WebContent/app/appModule/search.component.js
+++ BrokenTownsREST/WebContent/app/appModule/search.component.js
@@ -114,11 +114,30 @@ angular.module('appModule')
 			}
 			
 			vm.mapKey = "AIzaSyAM7sMRVwpJLTHY4KScoaPnpnjlZDRH3xg";
+			
+			vm.addMunicipalityToCaseItem = function(caseItem) {
+				municipalityService.index()
+					.then(function(res) {
+						res.data.forEach(function(val) {
+							val.caseItems.forEach(function(value) {
+								console.log("IN MUNICIPALITY TO CASE ITEM: ");
+								if (value.id == caseItem.id) {
+									console.log("FOUND IT" + vm.selected.municipality);
+									vm.selected.municipality = val;
+									return;
+								}
+							})
+						})
+					})
+			}
+			
+			
 
 			vm.applySelected = function(caseItem) {
 				
 			
 				vm.selected = caseItem;
+				vm.addMunicipalityToCaseItem(caseItem);
 				NgMap.getMap("map").then(
 						function(map) {
 							vm.map = map;
